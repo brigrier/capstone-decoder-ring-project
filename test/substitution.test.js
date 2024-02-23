057e6f9d8cf650aabc4287e4edd8aa2069db3594
Bri Grier <brianna.g6879@gmail.com>
2024-02-23 15:28:04 -0600

    Completion of substitution.test.js file

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,5 +1,5 @@
 // Write your tests here!
-/*
+
 const expect = require("chai").expect;
 const {substitution} = require("../src/substitution")
 
@@ -7,74 +7,82 @@ describe("substitution", () => {
     // error handling
     it("should return false if the substitution alphabet is missing", () => {
         
-        const input = 
-        const expected = 
-        const actual = 
+        const input = "pink"
+        const alphabet = ""
+        const expected = false
+        const actual = substitution(input, alphabet)
         expect(actual).to.eql(expected) 
         
     })
 
     it("should return false if the substitution alphabet is not exactly 26 characters", () => {
-        const input = 
-        const expected = 
-        const actual = 
+        const input = "pink"
+        const alphabet = "incodw98sj2k"
+        const expected = false
+        const actual = substitution(input, alphabet)
         expect(actual).to.eql(expected)
     })
 
     it("should return false if the substitution alphabet does not contain unique characters", () => {
-        const input = 
-        const expected = 
-        const actual = 
+        //const input = "pink"
+        const alphabet = "abcdefghijklmnopqrstuvwxyz"
+        const expected = false
+        const actual = substitution(alphabet)
         expect(actual).to.eql(expected)
     })
 
     //encoding a message
 
     it("should encode a message by using the given substitution alphabet", () => {
-        const input = 
-        const expected = 
-        const actual = 
+        const input = "hike"
+        const alphabet = "zyxwvutsrqponmlkjihgfedcba"
+        const expected = "srpv"
+        const actual = substitution(input, alphabet, encode = true)
         expect(actual).to.eql(expected)
     })
 
     it("should work with any kind of key with unique characters", () => {
-        const input = 
-        const expected = 
-        const actual = 
+        const input = "love"
+        const alphabet = "zyxwvutsrqponmlkjihgfedcba"
+        const expected = "olev"
+        const actual = substitution(input, alphabet, encode = true)
         expect(actual).to.eql(expected)
     })
 
     it("should preserve spaces", () => {
-        const input = 
-        const expected = 
-        const actual = 
+        const input = "hi me"
+        const alphabet = "zyxwvutsrqponmlkjihgfedcba"
+        const expected = "sr nv"
+        const actual = substitution(input, alphabet, encode = true)
         expect(actual).to.eql(expected)
     })
 
     //decoding a message
 
     it("should decode a message by using the given substitution alphabet", () => {
-        const input = 
-        const expected = 
-        const actual = 
+        const input = "srpv"
+        const alphabet = "zyxwvutsrqponmlkjihgfedcba"
+        const expected = "hike"
+        const actual = substitution(input, alphabet, encode = false)
         expect(actual).to.eql(expected)
     })
 
     it("should work with any kind of key with unique characters", () => {
-        const input = 
-        const expected = 
-        const actual = 
+        const input = "olev"
+        const alphabet = "zyxwvutsrqponmlkjihgfedcba"
+        const expected = "love"
+        const actual = substitution(input, alphabet, encode = false)
         expect(actual).to.eql(expected)
     })
 
     it("should preserve spaces", () => {
-       const input = 
-       const expected = 
-       const actual = 
+       const input = "sr nv"
+       const alphabet = "zyxwvutsrqponmlkjihgfedcba"
+       const expected = "hi me"
+       const actual = substitution(input, alphabet, encode = false)
        expect(actual).to.eql(expected)
     })
 
 
 })
 
-*/
